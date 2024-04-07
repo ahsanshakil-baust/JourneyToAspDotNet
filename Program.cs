1a4f3593e8ead5dc823aae0c273d79e68d7bd551
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2024-04-07 16:32:12 +0600

    first day

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PracticeOne
+{
+ public class Program{
+    static void Main(string[] args){
+        bool canVote=true;
+        
+        Console.WriteLine("Biggest Integer : {0}",int.MaxValue);
+        Console.WriteLine("Smallest Integer : {0}", int.MinValue);
+
+        Console.WriteLine("Biggest Long : {0}",long.MaxValue);
+        Console.WriteLine("Smallest Long : {0}",long.MinValue);
+
+        decimal decPiVal=3.1415565656556546545M;
+        decimal decBigNum=3.00000000000000011M;
+
+        Console.WriteLine("DEC : PI + bigNum = {0}",decPiVal+decBigNum);
+        
+    }
+ }   
+}
