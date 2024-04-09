ca615f6dcf0ef10fbd0ef9c93d8ef909d27f294e
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2024-04-09 16:34:10 +0600

    exercise One

--- Program.cs
+++ Program.cs
@@ -3,35 +3,18 @@ using System.Globalization;
 
 namespace PracticeOne
 {
- public class Program{
+    public class Program
+    {
 
-    static void PrintArray(int[] intArray, string mess){
-        foreach(int k in intArray){
-        Console.WriteLine("{0} : {1}",mess,k);
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a value : ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", i, value, i * value);
+            }
         }
-        
     }
-
-    static void Main(string[] args){
-        // Functions
-
-        int[] randNums={1,4,9,2};
-        PrintArray(randNums,"ForEach");
-
-        Array.Sort(randNums);
-        Array.Reverse(randNums);
-
-        const string textAge="-23";
-        int age=Convert.ToInt32(textAge);
-        Console.WriteLine("{0}",age);
-
-        double money=-10D / 3D;
-        
-        Console.WriteLine(money.ToString("C",CultureInfo.CurrentCulture));
-        Console.WriteLine(money.ToString("C",CultureInfo.CreateSpecificCulture("bn-BD")));
-
-        
-      
-    } 
- }   
 }
