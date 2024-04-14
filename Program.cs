f7b4f4fcd24425bfadc427363c971efd3ad31324
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2024-04-14 13:29:39 +0600

    array

--- Program.cs
+++ Program.cs
@@ -10,16 +10,34 @@ namespace PracticeOne
         {
             int[] numbers = new int[3];
 
-            Console.Write("Enter a number: ");
-            numbers[0] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter a number: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-            Console.Write("Enter a number: ");
-            numbers[1] = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("Enter a number: ");
+            // numbers[0] = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter a number: ");
-            numbers[2] = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("Enter a number: ");
+            // numbers[1] = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"{numbers[0]} {numbers[1]} {numbers[2]}");
+            // Console.Write("Enter a number: ");
+            // numbers[2] = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine($"{numbers[0]} {numbers[1]} {numbers[2]}");
+
+            // for (int i = 0; i < numbers.Length; i++)
+            // {
+            //     Console.WriteLine(numbers[i]);
+            //     Thread.Sleep(250);
+            // }
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+                Thread.Sleep(250);
+            }
         }
     }
 }
