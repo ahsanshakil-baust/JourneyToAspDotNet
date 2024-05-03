34fd903c02a72fbf04eb7a57560d3cfaf2e35237
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2024-05-03 19:34:34 +0600

    list

--- Program.cs
+++ Program.cs
@@ -8,35 +8,15 @@ namespace PracticeOne
 
         static void Main(string[] args)
         {
-            int[] numbers = new int[3];
-
-            for (int i = 0; i < numbers.Length; i++)
+            List<int> listNumber = new List<int>() { 1, 2, 3 };
+            foreach (int x in listNumber)
             {
-                Console.Write("Enter a number: ");
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("List : {0}", x);
             }
-
-            // Console.Write("Enter a number: ");
-            // numbers[0] = Convert.ToInt32(Console.ReadLine());
-
-            // Console.Write("Enter a number: ");
-            // numbers[1] = Convert.ToInt32(Console.ReadLine());
-
-            // Console.Write("Enter a number: ");
-            // numbers[2] = Convert.ToInt32(Console.ReadLine());
-
-            // Console.WriteLine($"{numbers[0]} {numbers[1]} {numbers[2]}");
-
-            // for (int i = 0; i < numbers.Length; i++)
-            // {
-            //     Console.WriteLine(numbers[i]);
-            //     Thread.Sleep(250);
-            // }
-
-            foreach (int num in numbers)
+            listNumber.RemoveAt(1);
+            foreach (int x in listNumber)
             {
-                Console.WriteLine(num);
-                Thread.Sleep(250);
+                Console.WriteLine("List : {0}", x);
             }
         }
     }
