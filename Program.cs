a132e17a6c6962f2938202cc8f258952e3bfdccb
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2024-05-03 19:54:22 +0600

    dictionary

--- Program.cs
+++ Program.cs
@@ -8,16 +8,47 @@ namespace PracticeOne
 
         static void Main(string[] args)
         {
-            List<int> listNumber = new List<int>() { 1, 2, 3 };
-            foreach (int x in listNumber)
+            Dictionary<int, string> names = new Dictionary<int, string>(){
+                {1,"Abbas"},
+                {2,"Sabbir"}
+            };
+
+            // names.Add(1, "Abbas");
+            // names.Add(2, "Sabbir");
+
+            // for (int i = 0; i < names.Count; i++)
+            // {
+            //     KeyValuePair<int, string> pair = names.ElementAt(i);
+            //     Console.WriteLine($"{pair.Key} - {pair.Value}");
+            // }
+
+
+            // if (names.TryGetValue(2, out string name))
+            // {
+            //     Console.WriteLine(name);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Not Found");
+            // }
+
+
+            if (names.ContainsKey(2))
             {
-                Console.WriteLine("List : {0}", x);
+                names.Remove(2);
             }
-            listNumber.RemoveAt(1);
-            foreach (int x in listNumber)
+            else
             {
-                Console.WriteLine("List : {0}", x);
+                Console.WriteLine("Not Found");
             }
+
+            foreach (KeyValuePair<int, string> item in names)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
