31127a752e23caa74648e7b044d551f2c08aea12
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2024-05-05 15:34:26 +0600

    return type

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace PracticeOne
@@ -9,24 +10,63 @@ namespace PracticeOne
         static string name = "Allu";
         static void Main(string[] args)
         {
-            int[] nums = new int[3];
+            // int[] nums = new int[3];
 
-            for (int i = 0; i < nums.Length; i++)
-            {
-                nums[i] = ReadNumber();
-            }
+            // int first = ReadNumber();
+            // int second = ReadNumber();
 
-            foreach (int x in nums)
-            {
-                Console.WriteLine($"{x}");
-            }
+            // Console.WriteLine(Add(a: second, b: first));
+
+
+            // for (int i = 0; i < nums.Length; i++)
+            // {
+            //     nums[i] = ReadNumber();
+            // }
+
+            // foreach (int x in nums)
+            // {
+            //     Console.WriteLine($"{x}");
+            // }
             // Console.WriteLine(PrintIntro());
+            string name = ReadUserDetails("Enter Your Name");
+            string age = ReadUserDetails("ENter Your Age");
+
+            Console.WriteLine(UserDetails(name, age));
+
+        }
+
+        // static int Add(int a, int b = default)
+        // {
+        //     return a + b;
+        // }
+
+
+        // static int Add(int a, int b = 0)
+        // {
+        //     return a + b;
+        // }
+
+
+        static int Add(int a, [Optional] int b)
+        {
+            return a + b;
+        }
+
+        static string UserDetails(string name, string age)
+        {
+            return $"Hello my name is {name} and my age is {age}";
         }
 
         static int ReadNumber()
         {
             Console.Write("Enter a Number : ");
             return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static string ReadUserDetails(string message)
+        {
+            Console.Write($"{message} : ");
+            return Console.ReadLine();
         }
 
         static string PrintIntro()
